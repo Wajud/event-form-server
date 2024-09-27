dd36875640d885bd7dfafcc61ccb61eb12073ac8
Wajud <wajudkareem757@gmail.com>
2024-09-28 00:32:43 +0100

    added a get all attendees route

--- controllers/registration.js
+++ controllers/registration.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Attendees = require("../models/registrationModel");
 
+router.get("/", async (req, res) => {
+  try {
+    const attendees = await Attendees.find();
+    res.status(200).json({ data: attendees });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Couldn't get attendees" });
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     console.log({ ...req.body });
