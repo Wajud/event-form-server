2d99c6252b8d604f2198572310e1e96a26043da3
Wajud <wajudkareem757@gmail.com>
2024-10-24 15:45:18 +0100

    added timestamps

--- models/registrationModel.js
+++ models/registrationModel.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
-const registModel = new mongoose.Schema({
-  fullName: String,
-  email: {
-    type: String,
-    unique: true,
+const registModel = new mongoose.Schema(
+  {
+    fullName: String,
+    email: {
+      type: String,
+      unique: true,
+    },
+    phoneNumber: String,
+    isMember: Boolean,
+    inAbuja: Boolean,
+    publicity: String,
   },
-  phoneNumber: String,
-  isMember: Boolean,
-  inAbuja: Boolean,
-  publicity: String,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const attendeesModel = mongoose.model("attendees", registModel);
 module.exports = attendeesModel;
